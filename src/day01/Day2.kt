c1a1cb652c2d8a64432c1e89e3d9d8c46567c428
Ajayvir S. <ajayvirsingh11@hotmail.com>
2025-12-02 15:33:44 +0100

    add day2B

--- src/day01/Day2.kt
+++ src/day01/Day2.kt
@@ -27,7 +27,47 @@ fun part1(text: String): Long{
         }
     }
     return totalSum
+}
 
+
+fun part2(text: String): Long {
+    // s=112, i=0
+    // store s[0] into res, while res.length <= s.length/2 || i<s.length:
+    // i++ -> for(j in res.length): res[j] != s[i+j] ? res = s[..i+j]
+    // localValid = false break
+    val rangesAsPairs = parseAsPairsList(text)
+    var totalSum: Long = 0
+    for (pair in rangesAsPairs) {
+        for (number in pair.first..pair.second) {
+            val stringNum = number.toString()
+            val strLen = stringNum.length
+            var invalid = false
+
+            for (blockLen in 1..strLen / 2) {
+                if(strLen % blockLen != 0){
+                    continue
+                }
+                var isValid = false
+                val block = stringNum.substring(0, blockLen)
+                for (i in blockLen..<strLen) {
+                    if(stringNum[i] != block[i % blockLen]){
+                        isValid = true
+                        break
+                    }
+                }
+
+                if(!isValid){
+                    invalid = true
+                    break
+                }
+            }
+            if(invalid){
+                totalSum+=number
+            }
+
+        }
+    }
+    return totalSum
 }
 
 fun parseAsPairsList(text: String): List<Pair<Long, Long>> {
@@ -47,7 +87,8 @@ fun main() {
             "Invalid format: '$text' (expected e.g. 22-30)"
     }
 
-    println(part1(text))
+    //println(part1(text))
+    println(part2(text))
 
 
 }
